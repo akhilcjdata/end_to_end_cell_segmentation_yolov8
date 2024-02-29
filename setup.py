f3bf0c03fb5714d71154577b29740df5e9e9c1ce
Akhil Janardhanan <akhil.janardhanan@vensure.com>
2024-02-29 10:37:03 +0530

    requirements_added_1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'cellSegmentation',
     version= '0.0.0',
     author= 'Akhil Janardhanan',
-    author_email= 'akhilcjdata@gmail',
+    author_email= 'akhilcjdata@gmail.com',
     packages= find_packages(),
     install_requires = []
 
